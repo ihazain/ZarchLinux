681411f79b80e36499a2d41751f41fd11e71a5f7
ihazain <zainalabdeen.ah@protonmail.com>
2026-03-05 03:24:29 +0100

    Remove custom keyring and pacman repo dependency

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -1,15 +1,6 @@
 if [[ -n ${ZARCHLINUX_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   zarch-pkg-add base-devel
-
-  # Configure pacman
-  sudo cp -f ~/.local/share/zarchlinux/default/pacman/pacman-${ZARCHLINUX_MIRROR:-stable}.conf /etc/pacman.conf
-  sudo cp -f ~/.local/share/zarchlinux/default/pacman/mirrorlist-${ZARCHLINUX_MIRROR:-stable} /etc/pacman.d/mirrorlist
-
-
-  sudo pacman -Sy
-  zarch-pkg-add zarchlinux-keyring
-
-  # Refresh all repos
+  # Refresh repos
   sudo pacman -Syyuu --noconfirm
 fi
